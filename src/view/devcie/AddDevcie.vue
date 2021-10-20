b3c1f444a9b78d96c3923fd91b826e72913e1f6a
yxb <964854596@qq.com>
2021-10-20 18:51:04 +0800

    前端

--- src/view/devcie/AddDevcie.vue
+++ src/view/devcie/AddDevcie.vue
@@ -1,10 +1,26 @@
 <template>
   <span>
-    <el-button :type="type" :size="size" :icon="icon" @click="showAddDialog">{{ title }}</el-button>
-    <el-col :span="24" class="warp-main" v-loading="loading" element-loading-text="拼命加载中">
+    <el-button :type="type" :size="size" :icon="icon" @click="showAddDialog">{{
+      title
+    }}</el-button>
+    <el-col
+      :span="24"
+      class="warp-main"
+      v-loading="loading"
+      element-loading-text="拼命加载中"
+    >
       <!--新增界面-->
-      <el-dialog :title="title" :visible.sync="addFormVisible" :close-on-click-modal="false">
-        <el-form :model="data" label-width="80px" :rules="addFormRules" ref="addForm">
+      <el-dialog
+        :title="title"
+        :visible.sync="addFormVisible"
+        :close-on-click-modal="false"
+      >
+        <el-form
+          :model="data"
+          label-width="80px"
+          :rules="addFormRules"
+          ref="addForm"
+        >
           <el-form-item label="设备ID" prop="deviceId">
             <el-input v-model="data.deviceId" auto-complete="off"></el-input>
           </el-form-item>
@@ -20,68 +36,67 @@
         </el-form>
         <div slot="footer" class="dialog-footer">
           <el-button @click.native="addFormVisible = false">取消</el-button>
-          <el-button type="primary" :loading="loading" @click.native="addSubmit">提交</el-button>
+          <el-button type="primary" :loading="loading" @click.native="addSubmit"
+            >提交</el-button
+          >
         </div>
       </el-dialog>
     </el-col>
   </span>
 </template>
 <script>
-import api from '../../api/api_device';
+import api from "../../api/api_device";
 
 export default {
   name: "AddDevcie",
-  //属性，父组件传参
   props: {
     //按钮图标
     icon: {
       type: String,
-      default: ''
+      default: "",
     },
     //按钮类型
     type: {
       type: String,
-      default: ''
+      default: "",
     },
     //按钮大小
     size: {
       type: String,
-      default: ''
+      default: "",
     },
     //编辑的参数
     data: {
       type: Object,
       default() {
         return {};
-      }
+      },
     },
     //执行成功回调
     call: {
       type: Function,
-      default: function () {
-      }
+      default: function () {},
     },
     //按钮、表单标题
     title: {
       type: String,
-      default: '新增'
-    }
+      default: "新增",
+    },
   },
   data() {
     return {
       loading: false,
       //新增相关数据
-      addFormVisible: false,//新增界面是否显示
+      addFormVisible: false, //新增界面是否显示
       addFormRules: {
         deviceId: [
-          {required: true, message: '请输入设备ID', trigger: 'blur'}
+          { required: true, message: "请输入设备ID", trigger: "blur" },
         ],
         isBind: [
-          {required: true, message: '请输入是否是否绑定', trigger: 'blur'}
+          { required: true, message: "请输入是否是否绑定", trigger: "blur" },
         ],
       },
-
-    }
+    };
   },
   methods: {
     showAddDialog: function () {
@@ -98,26 +113,42 @@ export default {
             // 编辑
             api.update(params, (result) => {
               that.loading = false;
-              if (result && result.code == '200') {
-                that.$message.success({showClose: true, message: '修改成功', duration: 2000});
-                that.$refs['addForm'].resetFields();
+              if (result && result.code == "200") {
+                that.$message.success({
+                  showClose: true,
+                  message: "修改成功",
+                  duration: 2000,
+                });
+                that.$refs["addForm"].resetFields();
                 that.addFormVisible = false;
                 that.call();
               } else {
-                that.$message. error({showClose: true, message: result.msg, duration: 2000});
+                that.$message.error({
+                  showClose: true,
+                  message: result.msg,
+                  duration: 2000,
+                });
               }
             });
           } else {
             //新增
             api.insert(params, (result) => {
               that.loading = false;
-              if (result && result.code == '200') {
-                that.$message.success({showClose: true, message: '新增成功', duration: 2000});
-                that.$refs['addForm'].resetFields();
+              if (result && result.code == "200") {
+                that.$message.success({
+                  showClose: true,
+                  message: "新增成功",
+                  duration: 2000,
+                });
+                that.$refs["addForm"].resetFields();
                 that.addFormVisible = false;
                 that.call();
               } else {
-                that.$message.error({showClose: true, message: result.msg, duration: 2000});
+                that.$message.error({
+                  showClose: true,
+                  message: result.msg,
+                  duration: 2000,
+                });
               }
             });
           }
@@ -125,8 +156,7 @@ export default {
       });
     },
   },
-  mounted() {
-  }
-}
+  mounted() {},
+};
 </script>
 
